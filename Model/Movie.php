ef68c8f86efdf86efb715e39b9d0308a5dd8c8a4
Simone Menichetti <simon3menichetti95@gmail.com>
2024-09-03 16:56:03 +0200

    fix ( construct error value , and sintax error class movie)

--- Model/Movie.php
+++ Model/Movie.php
@@ -1,72 +1,74 @@
 <?php
 
-// dichiarazione della Classe Movie
+    // dichiarazione della Classe Movie
 
-class Movie {
-    // titolo
-    public $title;
-    // lingua
-    public $language;
-    // anno di rilascio
-    public $releaseYear;
-    // genere
-    public $genre = [];
-    // votazione
-    public $rating;
-    // durata 
-    public $duration;
-    // incassi
-    public $boxOffice;
-    // regista
-    public $director;
-}
+    class Movie {
+        // titolo
+        public $title;
+        // lingua
+        public $language;
+        // anno di rilascio
+        public $releaseYear;
+        // genere
+        public $genre = [];
+        // votazione
+        public $rating;
+        // durata 
+        public $duration;
+        // incassi
+        public $boxOffice;
+        // regista
+        public $director;
 
-// manifestazione del costruttore 
-// Il costruttore permette di eseguire azioni nel momento in cui viene creata l’istanza della classe.
 
-function __construct($title,$language,$releaseYear,$genre,$rating,$duration,$boxOffice,$director){
+        // manifestazione del costruttore 
+        // Il costruttore permette di eseguire azioni nel momento in cui viene creata l’istanza della classe.
 
-    // uso di  $this per  accedere alle proprietà e ai metodi di quell'istanza specifica.
-    $this->title;
-    $this->language;
-    $this->releaseYear;
-    $this->genre;
-    $this->rating;
-    $this->$duration;
-    $this->$boxOffice;
-    $this->$director;
+        function __construct($title,$language,$releaseYear,$genre,$rating,$duration,$boxOffice,$director){
 
-}
-// creazione metodo get per ritornare i valori ed utilizzarli 
-public function getTitle(){
-    return $this->title;
-}
+            // uso di  $this per  accedere alle proprietà e ai metodi di quell'istanza specifica.
+            $this->title = $title;
+            $this->language = $language;
+            $this->releaseYear = $releaseYear;
+            $this->genre = $genre;
+            $this->rating = $rating;
+            $this->duration = $duration ;
+            $this->boxOffice = $boxOffice;
+            $this->director = $director;
 
-public function getLanguage(){
-    return $this->language;
-}
+        }
+        // creazione metodo get per ritornare i valori ed utilizzarli 
+        public function getTitle(){
+            return $this->title;
+        }
 
-public function getReleaseYear(){
-    return $this->releaseYear;
-}
+        public function getLanguage(){
+            return $this->language;
+        }
 
-public function getGenre(){
-    return $this->genre;
-}
+        public function getReleaseYear(){
+            return $this->releaseYear;
+        }
 
-public function getRating(){
-    return $this->rating;
-}
+        public function getGenre(){
+            return $this->genre;
+        }
 
-public function getDuration(){
-    return $this->duration;
-}
+        public function getRating(){
+            return $this->rating;
+        }
 
-public function getBoxOffice(){
-    return $this->boxOffice;
-}
+        public function getDuration(){
+            return $this->duration;
+        }
 
-public function getDirector(){
-    return $this->director; 
-}
+        public function getBoxOffice(){
+            return $this->boxOffice;
+        }
 
+        public function getDirector(){
+            return $this->director; 
+        }
+
+    }
+?>
